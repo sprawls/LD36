92521839ba3c47df6785419b0d77aa39c2bb727b
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-08-27 19:46:58 -0400

    added paddle prefab and model with eyes that follow player's head

---
+++ BreakoutVR/Assets/Scripts/EyeFollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeFollowPlayer : MonoBehaviour {
+
+    private Vector3 headPosition;
+    //public GameObject target;
+    public GameObject paddle;
+  
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        headPosition = HMDController.Instance.HeadsetPosition;
+
+        transform.LookAt(headPosition, Vector3.up);
+
+    }
+}
