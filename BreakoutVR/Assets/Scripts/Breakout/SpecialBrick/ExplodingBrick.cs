2683e9ce317aa943dd9db788c8e66b97bfa04186
Antoine Brouillette <antoineqc@hotmail.com>
2016-08-27 17:11:16 -0400

    Fix issue with UIScoreWindow, Linked brick to score + new types of bick

---
+++ BreakoutVR/Assets/Scripts/Breakout/SpecialBrick/ExplodingBrick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodingBrick : Brick
+{
+    [SerializeField]
+    private float m_explosionRadius = 0.5f;
+
+    protected override void Internal_OnDestroy()
+    {
+
+
+        base.Internal_OnDestroy();
+    }
+}
