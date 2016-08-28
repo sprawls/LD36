3f31fd702fbf2da18346833b4ea8c9ddc1475674
Antoine Brouillette <antoineqc@hotmail.com>
2016-08-27 22:15:48 -0400

    Grab and release things, still has some bugs

---
+++ BreakoutVR/Assets/Scripts/Player Related/Grabbable.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grabbable : MonoBehaviour
+{
+    public Rigidbody Body { get { return m_rigidbody; } }
+
+    private Rigidbody m_rigidbody;
+    public bool IsGrabbed { get; private set; }
+    public Transform OriginalParent { get; private set; }
+
+    void Awake()
+    {
+        m_rigidbody = GetComponentInChildren<Rigidbody>();
+        OriginalParent = transform.parent;
+        ResetRigidbodyState();
+    }
+
+    protected virtual void ResetRigidbodyState()
+    {
+        OnRelease();
+    }
+
+    public virtual void OnHoverStart(HMDController.ControllerIndex controller)
+    {
+        
+    }
+
+    public virtual void OnHoverEnd(HMDController.ControllerIndex controller)
+    {
+        
+    }
+
+    public virtual void OnGrab(HMDController.ControllerIndex controller)
+    {
+        m_rigidbody.useGravity = true;
+        IsGrabbed = true;
+    }
+
+    public virtual void OnRelease()
+    {
+        m_rigidbody.useGravity = true;
+        IsGrabbed = false;
+    }
+}
+
---
+++ BreakoutVR/Assets/Scripts/Player Related/HandController.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+[RequireComponent(typeof(SphereCollider))]
+public class HandController : MonoBehaviour
+{
+    [SerializeField]
+    private HMDController.ControllerIndex m_controller;
+
+    [SerializeField]
+    private Transform m_anchor;
+
+    //=============================================================================================
+
+    private List<Grabbable> m_grabbableInRange = new List<Grabbable>();
+    private Grabbable m_currentClosest = null;
+    private Grabbable m_grabbed = null;
+
+    //=============================================================================================
+    //------------------------------------------------------------------
+    [UsedImplicitly]
+    private void LateUpdate()
+    {
+        CheckClosestGrabbable();
+
+        if (HMDController.Instance.GetButtonDown(m_controller, HMDController.ControllerButton.Trigger))
+        {
+            if (m_grabbed == null)
+            {
+                RequestGrab();
+            }
+            else
+            {
+                RequestRelease();
+            }
+        }
+    }
+
+    //------------------------------------------------------------------
+    private void CheckClosestGrabbable()
+    {
+        if (m_grabbableInRange.Count == 0)
+            return;
+
+        if (m_grabbed != null)
+            return;
+
+        Grabbable closest = GameObjectUtils.GetClosest(m_grabbableInRange, this);
+        if (closest != m_currentClosest)
+        {
+            if (m_currentClosest != null)
+                m_currentClosest.OnHoverEnd(m_controller);
+            m_currentClosest = closest;
+            m_currentClosest.OnHoverStart(m_controller);
+        }
+    }
+
+    //------------------------------------------------------------------
+    private void RequestGrab()
+    {
+        if (m_currentClosest != null && m_grabbed == null)
+        {
+            m_grabbed = m_currentClosest;
+            m_grabbed.OnHoverEnd(m_controller);
+            m_grabbableInRange.Remove(m_currentClosest);
+            m_currentClosest = null;
+            m_grabbed.transform.SetParent(m_anchor);
+            m_grabbed.transform.localPosition = Vector3.zero;
+            m_grabbed.transform.localRotation = Quaternion.identity;
+            m_grabbed.OnGrab(m_controller);
+        }
+    }
+
+    //------------------------------------------------------------------
+    private void RequestRelease()
+    {
+        if (m_grabbed != null)
+        {
+            m_grabbed.transform.SetParent(m_grabbed.OriginalParent);
+            m_grabbed.OnRelease();
+            m_grabbed = null;
+        }
+    }
+
+    //------------------------------------------------------------------
+    [UsedImplicitly]
+    void OnTriggerEnter(Collider other)
+    {
+        Grabbable grabbable = other.GetComponent<Grabbable>();
+        if (grabbable != null && !grabbable.IsGrabbed && grabbable != m_grabbed)
+        {
+            m_grabbableInRange.Add(grabbable);
+        }
+    }
+
+    //------------------------------------------------------------------
+    [UsedImplicitly]
+    void OnTriggerExit(Collider other)
+    {
+        Grabbable grabbable = other.GetComponent<Grabbable>();
+        if (grabbable != null)
+        {
+            m_grabbableInRange.Remove(grabbable);
+        }
+    }
+}
