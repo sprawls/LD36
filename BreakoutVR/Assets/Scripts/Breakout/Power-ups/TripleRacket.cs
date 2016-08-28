e5522889b9fd87986305ffe0a1564c936c8bcee6
MarcAndreFL <marcfl20@gmail.com>
2016-08-28 10:52:39 -0400

    Ajout d'un effet kinda radial au outline des bricks + Debut des powerups

---
+++ BreakoutVR/Assets/Scripts/Breakout/Power-ups/TripleRacket.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TripleRacket : PowerUpAbstract
+{
+
+    [Header("TripleRacket Properties :")]
+    [SerializeField]
+    private float m_leftPaddleAngle = 45.0f;
+    [SerializeField]
+    private float m_rigtPaddleAngle = 45.0f;
+    [SerializeField]
+    private float m_animTime = 0.5f;
+    [SerializeField]
+    private float m_powerupTime = 10.0f;
+   
+
+    //Timers and 
+    public bool m_isActivated { get; private set; }
+    private float m_currentAnimTime = 0.0f;
+    private float m_currentPowerUPTime = 0.0f;
+
+
+
+    protected override void Start()
+    {
+	    
+	}
+
+    protected override void Update()
+    {
+        if (m_isActivated)
+        {
+            m_currentAnimTime += Time.deltaTime;
+            m_currentPowerUPTime += Time.deltaTime;
+            if (m_currentAnimTime < m_animTime)
+            {
+                //Anim over
+            }
+            if(m_currentPowerUPTime > m_powerupTime)
+            {
+                //It's over
+            }
+        }
+	}
+
+    protected override void Awake()
+    {
+
+    }
+
+    public override void Activate(HMDController.ControllerIndex cIndex)
+    {
+        m_isActivated = true;
+    }
+}
