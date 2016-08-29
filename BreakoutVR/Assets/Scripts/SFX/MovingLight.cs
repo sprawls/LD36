fd07b0e7ed44770a9148d5f4070bbfc1b8919eb3
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-08-28 21:02:33 -0400

    added SFXSpawner and a moving rainbow light that spawns from time to time

---
+++ BreakoutVR/Assets/Scripts/SFX/MovingLight.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingLight : MonoBehaviour
+{
+
+    private float speed = 7.0f;
+    private float duration = 1.35f;
+    private float intensityMin = 0f;
+    private float intensityMax = 1.5f;
+    public Gradient gradient;
+    public AnimationCurve intensityCurve;
+    private Light light;
+
+    void Awake()
+    {
+        light = gameObject.GetComponent<Light>();
+        StartCoroutine(MoveLightAndDestroy());
+    }
+
+    IEnumerator MoveLightAndDestroy()
+    {
+        for (float i = 0; i < 1.0f; i += Time.deltaTime / duration)
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+            light.color = gradient.Evaluate(i);
+            light.intensity = intensityCurve.Evaluate(i);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+
+
+
+
+
+    /*void Update()
+    {
+        moveLight();
+    }
+
+    public void moveLight()
+    {
+        transform.Translate(new Vector3(0, 0, speed));
+
+        float t = Mathf.Repeat(Time.time, duration) / duration;
+        gameObject.GetComponent<Light>().color = gradient.Evaluate(t);
+
+        StartCoroutine(destroyIfTimeUp());
+    }
+
+    IEnumerator destroyIfTimeUp()
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(gameObject);
+    }*/
+
+}
---
+++ BreakoutVR/Assets/Scripts/SFX/SFXSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFXSpawner : MonoBehaviour {
+
+    //Moving Light settings
+    public GameObject movingLightPrefab;
+    private GameObject movingLight;
+    private Vector3 spawnPosition;
+
+    void Start()
+    {
+        StartCoroutine(SpawnMovingLightPeriodically());
+    }
+
+    IEnumerator SpawnMovingLightPeriodically()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(3.0f, 8.0f));
+            spawnPosition = new Vector3(0, 1f, -2f);
+            movingLight = (GameObject)Instantiate(movingLightPrefab, spawnPosition, Quaternion.identity);
+        }
+
+    }
+
+    
+
+}
