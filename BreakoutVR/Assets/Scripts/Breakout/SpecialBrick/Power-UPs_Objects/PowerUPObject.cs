a3db52d6e797502a8896ba29ccca618f06d5c7dd
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-08-30 10:44:09 -0400

    paddle materials by me and SpecialBrick added files by not me

---
+++ BreakoutVR/Assets/Scripts/Breakout/SpecialBrick/Power-UPs_Objects/PowerUPObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUPObject : MonoBehaviour {
+
+    [SerializeField]
+    private PowerupType m_type;
+    [SerializeField]
+    private float m_speed=10.0f;
+    [SerializeField]
+    private Vector3 m_direction= -Vector3.forward;
+
+    void OnTriggerEnter(Collider col)
+    {
+        HandController hController = col.gameObject.GetComponentInParent<HandController>();
+        if(hController)
+        {
+            PowerupController.Instance.ActivatePowerUP(m_type, hController.controllerID);
+            Destroy(this.gameObject);
+        }
+        else if(col.tag == "KillZone")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        this.transform.position += Time.deltaTime * m_speed * m_direction;
+    }
+}
