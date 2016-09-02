c78d9ffa0172f6984fa2ff909c949232ade5e72b
sprawls <alex3521@hotmail.fr>
2016-09-01 20:43:00 -0400

    Cleaned up a bit, started UI for backwall

---
+++ BreakoutVR/Assets/Scripts/UI/UI_Backwall.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using GamejamToolset.LevelLoading;
+
+public class UI_Backwall : MonoBehaviour {
+
+    public GameObject ingameUI;
+    public GameObject gameNotStartedUI;
+    public GameObject gameOverUI;
+
+	// Use this for initialization
+	void Awake () {
+        ActivateGameNotStarted();
+    }
+
+    void Start() {
+        //LevelManager.OnLevelStart += OnLevelStart;
+	}
+
+    private void OnLevelStart(LevelName Level) {
+        //DO STUFF
+    }
+
+    private void DeactivateAll() {
+        ingameUI.SetActive(false);
+        gameNotStartedUI.SetActive(false);
+        gameOverUI.SetActive(false);
+    }
+
+    private void ActivateInGame() {
+        DeactivateAll();
+        ingameUI.SetActive(true);
+    }
+    private void ActivateGameNotStarted() {
+        DeactivateAll();
+        gameNotStartedUI.SetActive(true);
+    }
+    private void ActivateGameOver() {
+        DeactivateAll();
+        gameOverUI.SetActive(true);
+    }
+
+}
